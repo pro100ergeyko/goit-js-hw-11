2b7747215f24440fdfa3c3ba3cb52f732de7c9a9
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-06-28 21:00:55 +0300

    add js and html

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '37960229-568719668cd9d6c687eddc6ce';
