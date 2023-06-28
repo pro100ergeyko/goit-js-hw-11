0763abecd63e6fafd64eb830f6b26fd714c8b320
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-06-28 21:16:40 +0300

    change parcel

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import SimpleLightbox from 'simplelightbox';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const API_KEY = '37960229-568719668cd9d6c687eddc6ce';
